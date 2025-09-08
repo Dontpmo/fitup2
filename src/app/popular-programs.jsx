ec6abb9fe0e0671b4b1f78d543913578587555ac
Dontpmo <tspmo990@gmail.com>
2025-09-08 12:27:36 +0700

    Update popular-programs.jsx

--- src/app/popular-programs.jsx
+++ src/app/popular-programs.jsx
@@ -1,8 +1,9 @@
 "use client";
 import Carousel from "./carousel"
+import Plans from "./Plans";
 export default function PopularPrograms() {
     return (
-        <section id="programs" className="  bg-gradient-to-r from-lime-200 to-transparent py-13">
+        <section id="programs" className="  bg-radient from-goodbrown to-transparent py-13">
 
                 
                 <div className="text bg-lime-400 py-10 px-5  mx-3">
@@ -10,10 +11,11 @@ export default function PopularPrograms() {
                     <div className="flex items-center">
                         <p className="text-sm">PROGRAMS</p><div className="w-5 h-[1px] bg-black"></div>
                     </div>
-                    <h3 className="py-5 text-6xl font-bold">โปรแกรมขายดี</h3>
-                    <p >สิทธิพิเศษของสมาชิกยิมเรา ชวนเพื่อนๆและครอบครัวมาออกกำลังกายเป็นหมู่ไปกับเทรนเนอร์มืออาชีพ ฟรี! ได้ถึง4คน/1สมาชิก ดูตารางเวลาได้ด้านล่างเลย</p>
+                    <h3 className="py-5 text-6xl font-bold">รูปแบบสมาชิกที่เหมาะสมกับคุณ</h3>
+                    <p >ค้นหารูปแบบสมาชิกที่ยืดหยุ่นและเข้ากับไลฟ์สไตล์คุณ ค่าแรกเข้า 0 บาท</p>
                 </div>
-                <div className="px-3 my-13"><Carousel/></div>
+                <Plans/>
+               
         </section>
     )
 }
